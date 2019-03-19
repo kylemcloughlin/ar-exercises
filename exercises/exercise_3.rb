9d39cbcfd63a63385b0d22e522bc100b85fbec8e
kyle mcloughlin <kylemcloughlin1000@hotmail.ca>
2019-03-19 19:36:45 +0000

    destory a data file exercises completed

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -4,5 +4,11 @@ require_relative './exercise_2'
 
 puts "Exercise 3"
 puts "----------"
+puts " YOYOYOY #{Store.count}"
+
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+puts "YOYO #{Store.count}"
+
 
 # Your code goes here ...
